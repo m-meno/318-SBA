4e1eca586d5d24100a22b4a1773cb16ba851fc31
m-meno <mackenzie.meno@gmail.com>
2025-04-17 15:39:48 -0600

    worked on template engine

--- server.mjs
+++ server.mjs
@@ -4,6 +4,7 @@ import animalRoutes from "./routes/animalRoutes.mjs";
 import logReq from "./middleware/logRequest.mjs";
 import animals from "./data/animals.mjs";
 import enclosures from "./data/enslosures.mjs";
+import fs from "fs";
 
 //Setups
 const app = express();
@@ -16,6 +17,26 @@ app.use('/', animalRoutes);
 app.use(express.static("./styles"));
 app.use(logReq);
 
+// app.engine("template", (filePath, options, callback) => {
+//     fs.readFile(filePath, (err, content) => {
+//         if (err) return callback(err);
+//         const rendered = content
+//             .toString()
+//             .replaceAll("#title#", '${options.title}')
+//             .replace("#content#", '${options.}')
+//             .replace("#name#", '${options.}')
+//         return callback(null, rendered);    
+//     });
+// });
+
+// app.set("views", "./views");
+// app.set("view engine", "template");
+
+// app.get('/template', (req, res) => {
+//     let options = {title: "", contents: "", name: ""
+//     res.render("index", options);    
+//     }
+// });
 
 
 //Listener
